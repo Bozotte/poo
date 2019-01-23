4b9079ee6db30f5e59d64802d54304b4252e691b
Bozotte <armelle.peuvion.weiss@gmail.com>
2019-01-23 05:46:20 +0100

    ajouts csv

--- app.rb
+++ app.rb
@@ -1,10 +1,10 @@
 require 'bundler' # Brancher le Bundler au fichier app.rb > les require de gems deviennent inutiles mais le Gemfile doit être bien complété
 Bundler.require # #require 'lib/app/scrapper.rb'
 
-$:.unshift File.expand_path("./../lib", __FILE__)
-require 'lib/scrapper.rb'
+$:.unshift File.expand_path('./../lib', __FILE__)
 
-#Scrapper.new.perform |è Pour faire appel à la classe Scrapper (récupération données)
-#Scrapper.new.save_as_JSON
-Scrapper.new.save_as_spreadsheet
-#Scrapper.save_as_csv.perform
+require 'open-uri'
+require 'app/scrapper'
+
+scrap = Scrapper.new
+scrap.save_as_csv
--- lib/app/scrapper.rb
+++ lib/app/scrapper.rb
@@ -1,112 +1,90 @@
 
-# require 'bundler'
-# Bundler.require
+require 'bundler'
+Bundler.require
 # require 'nokogiri'
 # require 'rubygems'
 # require 'json'
 # require "google_drive"
+require 'pry'
+# require 'open_uri'
 
 class Scrapper
 
-def get_townhall_mails
-  townhall_mails = []
-  i = 0
+  attr_accessor :array_of_hash
+
+def initialize
+
+  @array_of_hash = []
+
+  townhall=[]
   page = Nokogiri::HTML(open("http://www.annuaire-des-mairies.com/val-d-oise.html"))
-  page.xpath('//p/a').each do |url|
-    mail = get_townhall_email("http://www.annuaire-des-mairies.com/"+url['href'][1..-1])
-#
-    townhall_mails << { url.text.downcase => mail }
+  page.xpath('//p/a').each do |name|
+      townhall << name.text
   end
 
-end
-
-def get_townhall_urls
-  urls = []
-  page = Nokogiri::HTML(open("http://www.annuaire-des-mairies.com/val-d-oise.html"))
-  page.xpath('//p/a').each do |url|
-    urls << { url.text.downcase => "http://www.annuaire-des-mairies.com/"+url['href'][1..-1] }
-  end
-  return urls
-end
-
-def get_townhall_email(townhall_url)
-  begin
-    page = Nokogiri::HTML(open(townhall_url))
-    #page.encoding = 'utf-8'
-    unless page.xpath('//body').text.downcase.include?('adresse email')
-      return ''
-    end
-    page.xpath('//td').each do |td|
-      match = td.text.match(/[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}/i)
-      if(match)
-        return match[0]
+  email = []
+  page = Nokogiri::HTML(open("http://annuaire-des-mairies.com/95/"))
+  news_links = page.css("a")
+  news_links.each do |link|
+      page_annuaire = Nokogiri::HTML(open("http://annuaire-des-mairies.com/95/#{link['href']}"))
+      page_annuaire.xpath('/html/body/div/main/section[2]/div/table/tbody/tr[4]/td[2]').each do |mail|
+          email << mail.text
       end
-    end
-    rescue StandardError => e
-      #ça permet de continuer d'exécuter et de voir de quelle erreur s'agit-il
-      #puts e.message
-      #puts e.backtrace.inspect
-      #puts townhall_url
-      return ''
   end
-  return ''
+
+  puts email
 end
 
-def big_array
-
-towns = get_townhall_urls
-
-a = []
-i=0
-towns.each do |town|
-  a << { town.keys[0] => get_townhall_email(town.values[0]) }
-  i += 1
-end
-
-return a
+  hash = Hash[townhall.zip(email.map)]
+  hash.each {|index| @array_of_hash << {index[0] => index[1]}}
 
 end
 
 def save_as_JSON # METHODE JSON #
-
-    File.open("db/mairie.json","w") do |f|
-    f.write(JSON.pretty_generate(big_array))
-
+     Dir.chdir ("db")
+     json_file = File.open("db/mairie.json","w") do |f|
+       f.write(@array_of_hash.to_json)
     end
 end
 
 def save_as_spreadsheet # METHODE SPREADSHEET #
+      session = GoogleDrive::Session.from_config("config.json")
+      new_sheet = session.spreadsheet_by_key("13Ey7nmVTcqG3egLjgV0oAvuXhsZIxK9tiz3KwuGamEo").worksheets[0]
+      new_sheet[1,1] = "Ville"
+      new_sheet[1, 2] = "E-mail"
+      initialize.each_key.with_index {|k, i| new_sheet[i+2,1] = k}
+      initialize.each_value.with_index {|v, i| new_sheet[i+2,2] = v}
+      new_sheet.save()
+      #binding.pry
+      end
 
-session = GoogleDrive::Session.from_config("config.json")
-ws = session.spreadsheet_by_key("13Ey7nmVTcqG3egLjgV0oAvuXhsZIxK9tiz3KwuGamEo").worksheets[0]
+    def save_as_csv # METHODE CSV | Voir https://www.rubyguides.com/2018/10/parse-csv-ruby/
+      Dir.chdir('db')
+      CSV.open("email.csv", "wb") do |csv| # Pour écrire fichier csv = file.write ou file.open + le code
+        @array_of_hash.each do |elements|
+          csv << [elements.keys.at(0), elements.values.at(0)]
+        end
+      end
+    end
 
-# Gets content of A2 cell.
-p ws[2, 1]  #==> "hoge"
-
-towns = get_townhall_urls
-
-  i=0
-  k=1
-  towns.each do |town|
-
-      ws[k, 1] = town.keys[0]
-      ws[k, 2] = get_townhall_email(town.values[0])
-      i += 1
-      k += 1
-  end
-
-  ws.save
-
-# Dumps all cells.
-(1..ws.num_rows).each do |row|
-  (1..ws.num_cols).each do |col|
-    p ws[row, col]
-  end
-end
-
-end
-
-def save_as_csv # CSV #
-
-
-end
+    def self.perform # METHODE SELF #
+  		puts "Welcome to Scrapper\n Tu veux le scrapping sous quel format ? JSON ? Spreadsheet ? CSV ?"
+  		print "> "
+  		choice = gets.to_i # entrée pour choisir le mode de scrapping des datas.
+      		case choice
+  		when 1
+  			Scrapper.new.save_as_json
+  			puts "JSON, bon choix"
+        sleep(5) "Le fichier se trouve ici : .../db"
+  		when 2
+  			Scrapper.new.save_as_spreadsheet
+  			puts "Ah, tu aimes Google ?, good for you"
+        sleep (5)
+        puts "Le fichier se trouve sur le Google drive à cette adresse  ..."
+  		when 3
+  			Scrapper.new.save_as_csv
+  			puts "CSV, old school but cool !"
+        sleep(5)
+        puts "Le fichier se trouve ici .../db"
+  		end
+  	end
